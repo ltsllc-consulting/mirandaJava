0444536b075913fc75837c410684fbf8d561ed0a
Clark Hobbie <clark.hobbie@gmail.com>
2017-06-05 06:30:22 -0600

    get/update user

--- src/main/java/com/ltsllc/mirandaClient/Session.java
+++ src/main/java/com/ltsllc/mirandaClient/Session.java
@@ -2,6 +2,7 @@ package com.ltsllc.mirandaClient;
 
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.ltsllc.miranda.PrivateKey;
 import com.ltsllc.miranda.PublicKey;
 import com.ltsllc.miranda.Results;
@@ -10,8 +11,11 @@ import com.ltsllc.miranda.servlet.login.LoginObject;
 import com.ltsllc.miranda.servlet.login.LoginResultObject;
 import com.ltsllc.miranda.servlet.objects.RequestObject;
 import com.ltsllc.miranda.servlet.objects.ResultObject;
+import com.ltsllc.miranda.servlet.user.GetUserResponseObject;
+import com.ltsllc.miranda.servlet.user.UserListResultObject;
 import com.ltsllc.miranda.servlet.user.UserObject;
 import com.ltsllc.miranda.servlet.user.UserRequestObject;
+import com.ltsllc.miranda.user.JSPublicKeyCreator;
 import com.ltsllc.miranda.user.User;
 import com.ltsllc.miranda.util.Utils;
 import org.apache.http.HttpResponse;
@@ -30,6 +34,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.log4j.Logger;
 
+import javax.crypto.KeyGenerator;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
@@ -38,13 +43,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
+import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 /**
  * Created by Clark on 6/2/2017.
  */
 public class Session {
-    private static Gson gson = new Gson();
+    public static class RetrieveUserResult {
+        public Results result;
+        public UserObject user;
+    }
+
+    private static Gson gson = buildGson();
+
     private static Logger logger = Logger.getLogger(Session.class);
 
     private String url;
@@ -58,6 +71,12 @@ public class Session {
         this.privateKey = privateKey;
         this.url = url;
         this.httpClient = createHttpClient();
+    }
+
+    public static Gson buildGson () {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(java.security.PublicKey.class, new JSPublicKeyCreator());
+        return gsonBuilder.create();
     }
 
     public String getSessionId() {
@@ -168,7 +187,7 @@ public class Session {
         }
     }
 
-    public boolean createUser (String name, String description, String category, String publicKeyPem) throws IOException {
+    public Results createUser (String name, String description, String category, String publicKeyPem) throws IOException {
         UserObject userObject = new UserObject(name, category, description, publicKeyPem);
         UserRequestObject userRequestObject = new UserRequestObject();
         userRequestObject.setUserObject(userObject);
@@ -179,12 +198,109 @@ public class Session {
         post.setEntity(stringEntity);
         HttpResponse httpResponse = getHttpClient().execute(post);
         ResultObject resultObject = getReply(httpResponse, ResultObject.class);
-        boolean result = false;
-        if (resultObject.getResult() == Results.Success) {
-            logger.info ("Created " + name);
-            result = true;
+
+        if (null == resultObject) {
+            return Results.SessionNotFound;
+        } else {
+            return resultObject.getResult();
+        }
+    }
+
+    public List<User> listUsers () throws IOException {
+        String url = getUrl() + "/servlets/getUsers";
+        UserRequestObject userRequestObject = new UserRequestObject();
+        userRequestObject.setSessionIdString(getSessionId());
+        HttpPost post = new HttpPost(url);
+        String json = gson.toJson(userRequestObject);
+        StringEntity stringEntity = new StringEntity(json);
+        post.setEntity(stringEntity);
+        HttpResponse httpResponse = getHttpClient().execute(post);
+        UserListResultObject userListResultObject = getReply(httpResponse, UserListResultObject.class);
+
+        if (null == userListResultObject) {
+            return null;
+        } else {
+            return userListResultObject.getUserList();
+        }
+    }
+
+    public Results deleteUser (String name) throws IOException {
+        String url = getUrl() + "/servlets/deleteUser";
+        UserRequestObject userRequestObject = new UserRequestObject();
+        userRequestObject.setSessionIdString(getSessionId());
+        UserObject userObject = new UserObject(name, null, null, null);
+        userRequestObject.setUserObject(userObject);
+        String json = gson.toJson(userRequestObject);
+
+        HttpPost httpPost = new HttpPost(url);
+        StringEntity stringEntity = new StringEntity(json);
+        httpPost.setEntity(stringEntity);
+
+        HttpResponse httpResponse = getHttpClient().execute(httpPost);
+        ResultObject resultObject = getReply(httpResponse, ResultObject.class);
+
+        if (null != resultObject) {
+            return resultObject.getResult();
+        } else {
+            return Results.SessionNotFound;
+        }
+    }
+
+    public Results updateUser (User user) throws IOException {
+        String url = getUrl() + "/servlets/updateUser";
+        UserRequestObject userRequestObject = new UserRequestObject();
+        userRequestObject.setSessionIdString(getSessionId());
+
+        UserObject userObject = new UserObject(user.getName(), user.getCategory().toString(), user.getDescription(),
+                user.getPublicKeyPem());
+
+        userRequestObject.setUserObject(userObject);
+
+        String json = gson.toJson(userRequestObject);
+        StringEntity stringEntity = new StringEntity(json);
+
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setEntity(stringEntity);
+
+        HttpResponse httpResponse = getHttpClient().execute(httpPost);
+        ResultObject resultObject = getReply(httpResponse, ResultObject.class);
+
+        if (resultObject == null) {
+            return Results.SessionNotFound;
+        } else {
+            return resultObject.getResult();
+        }
+    }
+
+
+    public RetrieveUserResult retrieveUser (String name) throws IOException {
+        String url = getUrl() + "/servlets/getUser";
+
+        UserRequestObject userRequestObject = new UserRequestObject();
+        userRequestObject.setSessionIdString(getSessionId());
+
+        UserObject userObject = new UserObject(name, null, null, null);
+        userRequestObject.setUserObject(userObject);
+
+        String json = gson.toJson(userRequestObject);
+
+        StringEntity stringEntity = new StringEntity(json);
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setEntity(stringEntity);
+
+        HttpResponse httpResponse = getHttpClient().execute(httpPost);
+
+        GetUserResponseObject getUserResponseObject = getReply(httpResponse, GetUserResponseObject.class);
+
+        RetrieveUserResult retrieveUserResult = new RetrieveUserResult();
+
+        if (null == getUserResponseObject) {
+            retrieveUserResult.result = Results.SessionNotFound;
+        } else {
+            retrieveUserResult.result = getUserResponseObject.getResult();
+            retrieveUserResult.user = getUserResponseObject.getUserObject();
         }
 
-        return result;
+        return retrieveUserResult;
     }
 }
--- src/test/java/com/ltsllc/mirandaClient/TestSession.java
+++ src/test/java/com/ltsllc/mirandaClient/TestSession.java
@@ -2,6 +2,7 @@ package com.ltsllc.mirandaClient;
 
 import com.ltsllc.miranda.PrivateKey;
 import com.ltsllc.miranda.PublicKey;
+import com.ltsllc.miranda.Results;
 import com.ltsllc.miranda.test.TestCase;
 import com.ltsllc.miranda.user.User;
 import com.ltsllc.miranda.util.Utils;
@@ -9,14 +10,22 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.KeyStore;
+import java.util.List;
 
 /**
  * Created by Clark on 6/2/2017.
  */
 public class TestSession extends TestCase {
+    public static class CreateResult {
+        public Results result;
+        public KeyPair keyPair;
+    }
+
     private Session session;
 
     public Session getSession() {
@@ -68,15 +77,101 @@ public class TestSession extends TestCase {
         assert (getSession().getLoggedIn());
     }
 
-    @Test
-    public void testCreateUser () throws Exception {
-        getSession().connect();
+
+    public boolean containsUser (String name, List<User> list) {
+        for (User user : list) {
+            if (name.equals(user.getName())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public CreateResult createUser (String name, String description, String category)
+            throws GeneralSecurityException, IOException{
         KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
         keyPairGenerator.initialize(2048);
         KeyPair keyPair = keyPairGenerator.generateKeyPair();
         PublicKey publicKey = new PublicKey(keyPair.getPublic());
         String publicKeyPem = Utils.publicKeyToPemString(publicKey.getSecurityPublicKey());
-        boolean result = getSession().createUser("stan", "a test user", "Publisher", publicKeyPem);
-        assert (result);
+
+        CreateResult createResult = new CreateResult();
+        createResult.keyPair = keyPair;
+        createResult.result = getSession().createUser(name, description, category, publicKeyPem);
+
+        return createResult;
+    }
+
+
+    @Test
+    public void testCreateUser () throws Exception {
+        getSession().connect();
+        CreateResult createResult = createUser("stan","a test user", User.UserTypes.Publisher.toString());
+        assert (createResult.result == Results.Success);
+
+        Results result = getSession().deleteUser("stan");
+        assert (result == Results.Success);
+    }
+
+    @Test
+    public void testListUsers () throws Exception {
+        getSession().connect();;
+        List<User> userList = getSession().listUsers();
+        assert (userList != null);
+    }
+
+    public Results performDelete (String name) throws IOException {
+        return getSession().deleteUser(name);
+    }
+
+    @Test
+    public void testDeleteUser () throws Exception {
+        getSession().connect();
+
+        CreateResult createResult = createUser("stan", "a test user", User.UserTypes.Publisher.toString());
+        assert (createResult.result == Results.Success);
+
+        Results result = getSession().deleteUser("stan");
+        assert (result == Results.Success);
+    }
+
+    public User createUser () throws GeneralSecurityException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        PublicKey publicKey = new PublicKey(keyPair.getPublic());
+        User user = new User("whatever", User.UserTypes.Publisher, "a test user", publicKey);
+        return user;
+    }
+
+    @Test
+    public void testModifyUser () throws Exception {
+        getSession().connect();
+
+        CreateResult createResult = createUser("stan", "a test user", User.UserTypes.Publisher.toString());
+
+        User user = createUser();
+        user.setName("stan");
+        user.setDescription("modify");
+
+        Results result = getSession().updateUser (user);
+        assert (result == Results.Success);
+
+        Session.RetrieveUserResult retrieveUserResult = getSession().retrieveUser("stan");
+        assert (retrieveUserResult.result == Results.Success);
+        assert (retrieveUserResult.user.getName().equals("stan"));
+        assert (retrieveUserResult.user.getDescription().equals("modify"));
+
+        result = getSession().deleteUser("stan");
+        assert (result == Results.Success);
+    }
+
+
+    public void whatever () throws Exception {
+        getSession().connect();
+        Results result = performDelete("stan");
+        assert (result == Results.Success);
     }
 }
